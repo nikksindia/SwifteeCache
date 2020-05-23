62a5aa51e227fa431e429eae2304265ade8d5c9c
Nikhil Sharma <nikksindia@gmail.com>
2020-05-23 12:37:26 +0530

    Changed framework name to SwifteeCache

---
+++ SwifteeCache/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by DMI on 23/05/20.
+//  Copyright © 2020 Nikhil Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
