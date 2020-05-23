7ca72f93079c014baa3377cf163a0501d7a66970
Nikhil Sharma <nikksindia@gmail.com>
2020-05-23 13:21:39 +0530

    Updated example project

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import SwifteeCache
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func storeProductDataToCache() {
+        let item1 = Product(name: "LXI 1",
+                            proudctId: 3237,
+                            productSKU: "MKL435678",
+                            price: 320.90)
+        SwifteeCache.sharedInstance.setObject(item1,forKey: "product")
+    }
+
+    func fetchProductDataFromCache() {
+      let result: Product? = SwifteeCache.sharedInstance.getObjectForKey("product")
+      debugPrint("Fetched result from cache \(String(describing: result))")
+    }
+    @IBAction func fetchDataButtonAction(_ sender: Any) {
+        fetchProductDataFromCache()
+    }
+    @IBAction func storeDataButtonAction(_ sender: Any) {
+        storeProductDataToCache()
+    }
 
 }
+
+
 
